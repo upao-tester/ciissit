fcefa8e7d458ee0253d291f2a647aa5bcff24da8
Martín Canaval <>
2013-11-14 12:34:24 -0500

    Creación de template

---
+++ maraton/src/main/java/upao/ciissit/maraton/Template.java
@@ -0,0 +1,52 @@
+package upao.ciissit.maraton;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author martin
+ */
+public abstract class Template {
+
+    private static final Logger l = Logger.getLogger(Template.class.getName());
+    
+    protected void init(String[] args) {
+        BufferedReader jin;
+        PrintStream jout;
+        try {
+            if (args.length == 2) {
+                jin = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+                jout = new PrintStream(new FileOutputStream(args[1]));
+            } else {
+                jin = new BufferedReader(new InputStreamReader(System.in));
+                jout = System.out;
+                l.log(Level.WARNING, "Por favor configurar entrada y salida!");
+                l.log(Level.WARNING, "Usando entrada y salida estándar.");
+            }
+            start(jin, jout);
+            jin.close();
+            jout.close();
+        } catch (FileNotFoundException ex) {
+            l.log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            l.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * La implementación de este método debe contener la solución a un problema
+     * 
+     * @param jin
+     * @param jout
+     * @throws IOException
+     */
+    public abstract void start(BufferedReader jin, PrintStream jout) throws IOException;
+}
