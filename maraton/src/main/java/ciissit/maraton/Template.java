37fc8c8999dd90adc689958c49b287133dfef512
upao-tester <>
2013-11-15 19:23:20 -0500

    Agregado modo testing con selección gráfica de archivo de entrada

--- maraton/src/main/java/ciissit/maraton/Template.java
+++ maraton/src/main/java/ciissit/maraton/Template.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -16,21 +17,37 @@ import java.util.logging.Logger;
 public abstract class Template {
 
     private static final Logger l = Logger.getLogger(Template.class.getName());
-    
+
     protected void init(String[] args) {
-        BufferedReader jin;
-        PrintStream jout;
         try {
+            BufferedReader jin;
+            PrintStream jout;
+            String inFileName = null;
+            String outFileName = null;
+
             if (args.length == 2) {
-                jin = new BufferedReader(new InputStreamReader(
-                        new FileInputStream(args[0])));
-                jout = new PrintStream(args[1], "UTF-8");
+                l.log(Level.INFO, "Entrada y salida correctamente declaradas!");
+                inFileName = args[0];
+                outFileName = args[1];
             } else {
-                // TODO usar filechooser para modo de prueba
+                l.log(Level.WARNING, "Estableciendo entrada y salida!");
+                // Modo Testing
+                JFileChooser fs = new JFileChooser();
+                int resp = fs.showOpenDialog(null);
+                if (resp == JFileChooser.APPROVE_OPTION) {
+                    inFileName = fs.getSelectedFile().getPath();
+                    outFileName = inFileName.replaceAll("\\..*$", ".out");
+                    l.log(Level.INFO, "Salida: {0}", outFileName);
+                }
+            }
+            if (inFileName != null && outFileName != null) {
+                jin = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(inFileName)));
+                jout = new PrintStream(outFileName, "UTF-8");
+            } else {
+                l.log(Level.WARNING, "Usando entrada y salida estándar.");
                 jin = new BufferedReader(new InputStreamReader(System.in));
                 jout = System.out;
-                l.log(Level.WARNING, "Por favor configurar entrada y salida!");
-                l.log(Level.WARNING, "Usando entrada y salida estándar.");
             }
             start(jin, jout);
             jin.close();
@@ -44,10 +61,11 @@ public abstract class Template {
 
     /**
      * La implementación de este método debe contener la solución a un problema
-     * 
+     *
      * @param jin
      * @param jout
      * @throws IOException
      */
-    public abstract void start(BufferedReader jin, PrintStream jout) throws IOException;
+    public abstract void start(BufferedReader jin, PrintStream jout)
+            throws IOException;
 }
